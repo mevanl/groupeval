ebc3930c45cbe4bd715c28587af72ce7070a32c5
mevanl <mevanlauderdaleyt@gmail.com>
2025-04-16 14:10:08 -0500

    Added comments for psuedocode

--- server.js
+++ server.js
@@ -17,12 +17,22 @@ app.get("*", (request, response, next) => {
 // LOGIN AND REGISTER API 
 // validate login 
 app.get("/api/validate_login", (request, response, next) => {
+    // get request body information (email and password)
 
+    // validate
+    
+    // db.rin 
 })
 
 // register account 
 app.post("/api/register_account", (request, response, next) => {
+    // get request body information 
 
+    // validate with regex and NIST standard 
+
+    // check db if email exists yet or not 
+
+    // if doesnt exists and validation successful, db.run 
 })
 
 
@@ -30,28 +40,64 @@ app.post("/api/register_account", (request, response, next) => {
 // get the courses you are in 
 app.get("/api/get_courses", (request, response, next) => {
     // can return an object of courses with two main sections, teaching and enrolled based on the role 
+
+    
+    // get request body information (the user)
+
+    // check if user exists in db (redudant)
+
+    // if exists, db.run there courses 
 })
 
 
 // COURSE API 
 // get peer review assignments for a class 
 app.get("/api/get_peer_reviews", (request, response, next) => {
+    // get request body information (course name)
 
+    // validate request body information 
+
+    // check to make sure course exists in db (redundant but better safe)
+
+    // if validation good, db.run 
 })
 
 // teacher create peer review for a class 
 app.post("/api/create_peer_review", (request, response, next) => {
-    
+    // get request body information (course name, peer review data)
+
+    // validate request body information 
+
+    // check to make sure course exists in db (redundant but better safe)
+
+    // check that peer review name is unqiue in db 
+
+    // if validation good, db.run  
 })
 
 // get course groups 
 app.get("/api/get_groups", (request, response, next) => {
+    // get request body information (course name)
 
+    // validate request body information 
+
+    // check to make sure course exists in db (redundant but better safe)
+
+    // if course exists, db.run for groups in that course 
 })
 
 // teacher create group for class
 app.post("/api/create_group", (request, response, next) => {
+    // get request body information 
 
+    // validation request body information
+
+    // check in database course exists in db (redundant but better safe)
+
+    // check in database if this group conflicts in this course 
+        // (group name should be unique basically)
+    
+    // If unqiue and validated successfully, db.run 
 })
 
 
