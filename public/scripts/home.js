b244767c217480e09c7ebc64842404609d98e3a4
evan <mevanlauderdaleyt@gmail.com>
2025-05-04 17:03:32 -0500

    Put default func on home

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,9 +1,14 @@
 import { load_page } from "../app.js"
 
-document.querySelector("#button_login").addEventListener("click", function(event) {
-    load_page("/login")
-})
 
-document.querySelector('#button_register').addEventListener("click", function(event) {
-    load_page("/register")
-})
+export default function Home() {
+
+    document.querySelector("#button_login").addEventListener("click", function(event) {
+        load_page("/login")
+    })
+
+    document.querySelector('#button_register').addEventListener("click", function(event) {
+        load_page("/register")
+    })
+
+}
